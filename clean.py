390c3c0e1a2555d05084e96decc61e965a429328
Jason Kao <jason.kao@columbia.edu>
2021-03-04 15:32:49 -0500

    r

--- clean.py
+++ clean.py
@@ -1,9 +1,5 @@
-import json
 import sys
 
-str_data = (
-    sys.stdin.readlines()[1][3:-3]
-    .replace("id=", '"id":')
-    .replace("mean=", '"mean":')
+print(
+    sys.stdin.readlines()[1][3:-3].replace("id=", '"id":').replace("mean=", '"mean":')
 )
-print(str_data)
