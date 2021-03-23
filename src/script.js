911d8fe63553a3d478850cd13ac27f0faa0f4225
Jason Kao <jason.kao@columbia.edu>
2021-03-23 18:40:45 -0400

    stuff

--- src/script.js
+++ src/script.js
@@ -3,14 +3,15 @@ import SVGInject from '@iconfu/svg-inject';
 import reliefs from '../data/reliefs/*.png';
 import basemaps from '../data/basemaps/*.png';
 import boundaries from '../data/boundaries/*.svg';
+import charts from '../data/charts/*.png';
 
-import tukeyResults from '../data/tukey-results.json';
+import tukeyResults from '../data/tukeyResults.json';
 
-const cities = Object.keys(reliefs).map(filename => filename.split('.')[0])
+const cities = Object.keys(reliefs);
 
 /* Dropdown setup */
 
-const citySelector = document.getElementById('city-selector');
+const citySelector = document.getElementById('city-select');
 cities.forEach(city => {
   const option = document.createElement('option');
   option.value = option.innerHTML = city;
@@ -23,8 +24,9 @@ function setCityMap(city) {
   const map = document.getElementById('map');
   const [boundarySvg, basemapImg, reliefImg] = map.children;
   const boundaryImg = document.createElement('img');
+  boundaryImg.setAttribute('onload', 'SVGInject(this)');
   boundaryImg.src = boundaries[city];
-  SVGInject(boundaryImg);
+  // SVGInject(boundaryImg);
   map.replaceChild(boundaryImg, boundarySvg);
   reliefImg.src = reliefs[city];
   basemapImg.src = basemaps[city];
@@ -36,11 +38,19 @@ function setCityTable(city) {
   const results = tukeyResults[city]
 }
 
+/* Chart */
+
+function setCityChart(city) {
+  document.getElementById('chart').src = charts[city];
+  document.getElementById('chart-tky').src = charts[city + '-tky'];
+}
+
 /* Dropdown listeners */
 
 function setCity(city) {
   setCityMap(city);
   setCityTable(city);
+  setCityChart(city);
 }
 
 citySelector.addEventListener('change', function () {
