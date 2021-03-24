942ca4fd39388eb87a7c2436e6f7a036d78ca709
Jason Kao <jason.kao@columbia.edu>
2021-03-23 23:59:47 -0400

    Deploy to gh-pages

---
+++ script.75da7f30.js
@@ -0,0 +1,136 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"f1GN":[function(require,module,exports) {
+module.exports="Atlanta-2000.8f9d2946.png";
+},{}],"XvR1":[function(require,module,exports) {
+module.exports="Atlanta-2020.4f1257fa.png";
+},{}],"P5tt":[function(require,module,exports) {
+module.exports="Austin-2000.f22fa9e9.png";
+},{}],"bLwB":[function(require,module,exports) {
+module.exports="Austin-2020.8f452ee1.png";
+},{}],"PstV":[function(require,module,exports) {
+module.exports="Baltimore-2000.25a25b3f.png";
+},{}],"Pv1Z":[function(require,module,exports) {
+module.exports="Baltimore-2020.718cb53c.png";
+},{}],"LDMD":[function(require,module,exports) {
+module.exports="Chicago-2000.c00c3fe1.png";
+},{}],"rXRG":[function(require,module,exports) {
+module.exports="Chicago-2020.1daac48e.png";
+},{}],"Pcyl":[function(require,module,exports) {
+module.exports="Detroit-2000.1356c410.png";
+},{}],"kNrH":[function(require,module,exports) {
+module.exports="Detroit-2020.70e058be.png";
+},{}],"NEXW":[function(require,module,exports) {
+module.exports="Houston-2000.64d6bbd6.png";
+},{}],"xYDg":[function(require,module,exports) {
+module.exports="Houston-2020.fbeef959.png";
+},{}],"wVYJ":[function(require,module,exports) {
+module.exports="New_Orleans-2000.a69587a2.png";
+},{}],"RYR2":[function(require,module,exports) {
+module.exports="New_Orleans-2020.47ab9712.png";
+},{}],"j8Oj":[function(require,module,exports) {
+module.exports="Philadelphia-2000.70827460.png";
+},{}],"QlMW":[function(require,module,exports) {
+module.exports="Philadelphia-2020.bdb14b7a.png";
+},{}],"meqU":[function(require,module,exports) {
+module.exports="Portland-2000.575425a7.png";
+},{}],"EayI":[function(require,module,exports) {
+module.exports="Portland-2020.a8a215b8.png";
+},{}],"vBdG":[function(require,module,exports) {
+module.exports="Richmond-2000.abc6b2c0.png";
+},{}],"rC2s":[function(require,module,exports) {
+module.exports="Richmond-2020.bee4d110.png";
+},{}],"RrJH":[function(require,module,exports) {
+module.exports={"Atlanta-2000":require("./Atlanta-2000.png"),"Atlanta-2020":require("./Atlanta-2020.png"),"Austin-2000":require("./Austin-2000.png"),"Austin-2020":require("./Austin-2020.png"),"Baltimore-2000":require("./Baltimore-2000.png"),"Baltimore-2020":require("./Baltimore-2020.png"),"Chicago-2000":require("./Chicago-2000.png"),"Chicago-2020":require("./Chicago-2020.png"),"Detroit-2000":require("./Detroit-2000.png"),"Detroit-2020":require("./Detroit-2020.png"),"Houston-2000":require("./Houston-2000.png"),"Houston-2020":require("./Houston-2020.png"),"New_Orleans-2000":require("./New_Orleans-2000.png"),"New_Orleans-2020":require("./New_Orleans-2020.png"),"Philadelphia-2000":require("./Philadelphia-2000.png"),"Philadelphia-2020":require("./Philadelphia-2020.png"),"Portland-2000":require("./Portland-2000.png"),"Portland-2020":require("./Portland-2020.png"),"Richmond-2000":require("./Richmond-2000.png"),"Richmond-2020":require("./Richmond-2020.png")};
+},{"./Atlanta-2000.png":"f1GN","./Atlanta-2020.png":"XvR1","./Austin-2000.png":"P5tt","./Austin-2020.png":"bLwB","./Baltimore-2000.png":"PstV","./Baltimore-2020.png":"Pv1Z","./Chicago-2000.png":"LDMD","./Chicago-2020.png":"rXRG","./Detroit-2000.png":"Pcyl","./Detroit-2020.png":"kNrH","./Houston-2000.png":"NEXW","./Houston-2020.png":"xYDg","./New_Orleans-2000.png":"wVYJ","./New_Orleans-2020.png":"RYR2","./Philadelphia-2000.png":"j8Oj","./Philadelphia-2020.png":"QlMW","./Portland-2000.png":"meqU","./Portland-2020.png":"EayI","./Richmond-2000.png":"vBdG","./Richmond-2020.png":"rC2s"}],"t2Js":[function(require,module,exports) {
+module.exports="Atlanta.4c263e71.png";
+},{}],"RzKN":[function(require,module,exports) {
+module.exports="Austin.a90e194d.png";
+},{}],"TNfA":[function(require,module,exports) {
+module.exports="Baltimore.0988618f.png";
+},{}],"kHpz":[function(require,module,exports) {
+module.exports="Chicago.7656e0bf.png";
+},{}],"Ppq2":[function(require,module,exports) {
+module.exports="Detroit.a1722c1d.png";
+},{}],"GJh8":[function(require,module,exports) {
+module.exports="Houston.b45be159.png";
+},{}],"AAGZ":[function(require,module,exports) {
+module.exports="New_Orleans.41754b71.png";
+},{}],"yxfC":[function(require,module,exports) {
+module.exports="Philadelphia.7360c70e.png";
+},{}],"XOb4":[function(require,module,exports) {
+module.exports="Portland.382c2b33.png";
+},{}],"IwRW":[function(require,module,exports) {
+module.exports="Richmond.d8c17bd8.png";
+},{}],"oRTp":[function(require,module,exports) {
+module.exports={Atlanta:require("./Atlanta.png"),Austin:require("./Austin.png"),Baltimore:require("./Baltimore.png"),Chicago:require("./Chicago.png"),Detroit:require("./Detroit.png"),Houston:require("./Houston.png"),New_Orleans:require("./New_Orleans.png"),Philadelphia:require("./Philadelphia.png"),Portland:require("./Portland.png"),Richmond:require("./Richmond.png")};
+},{"./Atlanta.png":"t2Js","./Austin.png":"RzKN","./Baltimore.png":"TNfA","./Chicago.png":"kHpz","./Detroit.png":"Ppq2","./Houston.png":"GJh8","./New_Orleans.png":"AAGZ","./Philadelphia.png":"yxfC","./Portland.png":"XOb4","./Richmond.png":"IwRW"}],"wqQY":[function(require,module,exports) {
+module.exports="Atlanta.93bedc43.svg";
+},{}],"sA22":[function(require,module,exports) {
+module.exports="Austin.97b11d3b.svg";
+},{}],"gpS0":[function(require,module,exports) {
+module.exports="Baltimore.21219727.svg";
+},{}],"Qr5k":[function(require,module,exports) {
+module.exports="Chicago.66f9371b.svg";
+},{}],"iV1Y":[function(require,module,exports) {
+module.exports="Detroit.92245d9f.svg";
+},{}],"Y6Tr":[function(require,module,exports) {
+module.exports="Houston.79969b88.svg";
+},{}],"jB2x":[function(require,module,exports) {
+module.exports="New_Orleans.a67256b9.svg";
+},{}],"oWG7":[function(require,module,exports) {
+module.exports="Philadelphia.26f7214a.svg";
+},{}],"UsGl":[function(require,module,exports) {
+module.exports="Portland.3808357d.svg";
+},{}],"EufJ":[function(require,module,exports) {
+module.exports="Richmond.a3239b80.svg";
+},{}],"fH6a":[function(require,module,exports) {
+module.exports={Atlanta:require("./Atlanta.svg"),Austin:require("./Austin.svg"),Baltimore:require("./Baltimore.svg"),Chicago:require("./Chicago.svg"),Detroit:require("./Detroit.svg"),Houston:require("./Houston.svg"),New_Orleans:require("./New_Orleans.svg"),Philadelphia:require("./Philadelphia.svg"),Portland:require("./Portland.svg"),Richmond:require("./Richmond.svg")};
+},{"./Atlanta.svg":"wqQY","./Austin.svg":"sA22","./Baltimore.svg":"gpS0","./Chicago.svg":"Qr5k","./Detroit.svg":"iV1Y","./Houston.svg":"Y6Tr","./New_Orleans.svg":"jB2x","./Philadelphia.svg":"oWG7","./Portland.svg":"UsGl","./Richmond.svg":"EufJ"}],"KdYd":[function(require,module,exports) {
+module.exports="Atlanta-tky.3a16b468.png";
+},{}],"y7qZ":[function(require,module,exports) {
+module.exports="Atlanta.9c28dda6.png";
+},{}],"nc54":[function(require,module,exports) {
+module.exports="Austin-tky.9856140b.png";
+},{}],"yzta":[function(require,module,exports) {
+module.exports="Austin.ba71d0c1.png";
+},{}],"bN6B":[function(require,module,exports) {
+module.exports="Baltimore-tky.9f8502b1.png";
+},{}],"kEga":[function(require,module,exports) {
+module.exports="Baltimore.8c012ec6.png";
+},{}],"WEXj":[function(require,module,exports) {
+module.exports="Chicago-tky.b9746462.png";
+},{}],"GJhw":[function(require,module,exports) {
+module.exports="Chicago.3d6f2a99.png";
+},{}],"tY3G":[function(require,module,exports) {
+module.exports="Detroit-tky.5515faaf.png";
+},{}],"YYpL":[function(require,module,exports) {
+module.exports="Detroit.47ba0c48.png";
+},{}],"aFku":[function(require,module,exports) {
+module.exports="Houston-tky.f31577b8.png";
+},{}],"ndRX":[function(require,module,exports) {
+module.exports="Houston.16368566.png";
+},{}],"wW0I":[function(require,module,exports) {
+module.exports="New_Orleans-tky.5f56f75a.png";
+},{}],"nsfZ":[function(require,module,exports) {
+module.exports="New_Orleans.37f903d7.png";
+},{}],"imLN":[function(require,module,exports) {
+module.exports="Philadelphia-tky.2b3c7727.png";
+},{}],"i1ug":[function(require,module,exports) {
+module.exports="Philadelphia.a7dd2a72.png";
+},{}],"YvqV":[function(require,module,exports) {
+module.exports="Portland-tky.11a8c3f3.png";
+},{}],"IOKP":[function(require,module,exports) {
+module.exports="Portland.d7486471.png";
+},{}],"J8OG":[function(require,module,exports) {
+module.exports="Richmond-tky.7fcb0bc4.png";
+},{}],"AXGM":[function(require,module,exports) {
+module.exports="Richmond.2dd7ebfb.png";
+},{}],"X6JI":[function(require,module,exports) {
+module.exports="nation-tky.ffe379e4.png";
+},{}],"JUXX":[function(require,module,exports) {
+module.exports="nation.b74660ae.png";
+},{}],"eSI4":[function(require,module,exports) {
+module.exports={"Atlanta-tky":require("./Atlanta-tky.png"),Atlanta:require("./Atlanta.png"),"Austin-tky":require("./Austin-tky.png"),Austin:require("./Austin.png"),"Baltimore-tky":require("./Baltimore-tky.png"),Baltimore:require("./Baltimore.png"),"Chicago-tky":require("./Chicago-tky.png"),Chicago:require("./Chicago.png"),"Detroit-tky":require("./Detroit-tky.png"),Detroit:require("./Detroit.png"),"Houston-tky":require("./Houston-tky.png"),Houston:require("./Houston.png"),"New_Orleans-tky":require("./New_Orleans-tky.png"),New_Orleans:require("./New_Orleans.png"),"Philadelphia-tky":require("./Philadelphia-tky.png"),Philadelphia:require("./Philadelphia.png"),"Portland-tky":require("./Portland-tky.png"),Portland:require("./Portland.png"),"Richmond-tky":require("./Richmond-tky.png"),Richmond:require("./Richmond.png"),"nation-tky":require("./nation-tky.png"),nation:require("./nation.png")};
+},{"./Atlanta-tky.png":"KdYd","./Atlanta.png":"y7qZ","./Austin-tky.png":"nc54","./Austin.png":"yzta","./Baltimore-tky.png":"bN6B","./Baltimore.png":"kEga","./Chicago-tky.png":"WEXj","./Chicago.png":"GJhw","./Detroit-tky.png":"tY3G","./Detroit.png":"YYpL","./Houston-tky.png":"aFku","./Houston.png":"ndRX","./New_Orleans-tky.png":"wW0I","./New_Orleans.png":"nsfZ","./Philadelphia-tky.png":"imLN","./Philadelphia.png":"i1ug","./Portland-tky.png":"YvqV","./Portland.png":"IOKP","./Richmond-tky.png":"J8OG","./Richmond.png":"AXGM","./nation-tky.png":"X6JI","./nation.png":"JUXX"}],"mpVp":[function(require,module,exports) {
+"use strict";var e=a(require("../data/reliefs/*.png")),t=a(require("../data/basemaps/*.png")),n=a(require("../data/boundaries/*.svg")),r=a(require("../data/charts/*.png"));function a(e){return e&&e.__esModule?e:{default:e}}function o(e,t){return c(e)||u(e,t)||f(e,t)||i()}function i(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function u(e,t){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e)){var n=[],r=!0,a=!1,o=void 0;try{for(var i,u=e[Symbol.iterator]();!(r=(i=u.next()).done)&&(n.push(i.value),!t||n.length!==t);r=!0);}catch(c){a=!0,o=c}finally{try{r||null==u.return||u.return()}finally{if(a)throw o}}return n}}function c(e){if(Array.isArray(e))return e}function l(e){return m(e)||s(e)||f(e)||d()}function d(){throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function f(e,t){if(e){if("string"==typeof e)return y(e,t);var n=Object.prototype.toString.call(e).slice(8,-1);return"Object"===n&&e.constructor&&(n=e.constructor.name),"Map"===n||"Set"===n?Array.from(e):"Arguments"===n||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?y(e,t):void 0}}function s(e){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e))return Array.from(e)}function m(e){if(Array.isArray(e))return y(e)}function y(e,t){(null==t||t>e.length)&&(t=e.length);for(var n=0,r=new Array(t);n<t;n++)r[n]=e[n];return r}var v=l(new Set(Object.keys(e.default).map(function(e){return e.split("-")[0]}))),h=document.getElementById("map"),p=document.getElementById("city-select"),b=document.getElementById("year-select");function g(e,t){I(e,t),S(e)}function E(t){h.children[2].src=e.default[p.value+"-"+t]}function I(r,a){var i=o(h.children,3),u=i[0],c=i[1],l=i[2],d=document.createElement("img");d.setAttribute("onload","SVGInject(this)"),d.src=n.default[r],h.replaceChild(d,u),l.src=e.default[r+"-"+a],c.src=t.default[r]}function S(e){var t=arguments.length>1&&void 0!==arguments[1]?arguments[1]:document.getElementById("chart"),n=arguments.length>2&&void 0!==arguments[2]?arguments[2]:document.getElementById("chart-tky");t.src=r.default[e],n.src=r.default[e+"-tky"]}[[p,v],[b,[2e3,2020]]].forEach(function(e){var t=o(e,2),n=t[0];t[1].forEach(function(e){var t=document.createElement("option");t.value=e,t.innerHTML=(""+e).replace("_"," "),n.append(t)})}),p.addEventListener("change",function(){g(this.value,b.value)}),b.addEventListener("change",function(){E(this.value)}),document.addEventListener("DOMContentLoaded",function(){p.value="Baltimore",b.value=2020,p.dispatchEvent(new Event("change")),S("nation",document.getElementById("nation-chart"),document.getElementById("nation-chart-tky"))});
+},{"../data/reliefs/*.png":"RrJH","../data/basemaps/*.png":"oRTp","../data/boundaries/*.svg":"fH6a","../data/charts/*.png":"eSI4"}]},{},["mpVp"], "script")
+//# sourceMappingURL=script.75da7f30.js.map
