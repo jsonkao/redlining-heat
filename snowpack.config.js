87a82d47b09c2ab73f894fc0f73bfd3b82121a65
Jason Kao <jason.kao@columbia.edu>
2021-03-26 17:46:32 -0400

    gh-pages

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,5 +4,6 @@ module.exports = {
   exclude: [
     '/Users/jasonkao/Development/redlining-heat/data/**/*.{tif,topojson,osm,json}',
     '/Users/jasonkao/Development/redlining-heat/data/Makefile',
+    '/Users/jasonkao/Development/redlining-heat/src/partials/*.html',
   ],
 };
