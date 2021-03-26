419d828845190ddcc130fa33b74fbed760e70693
Jason Kao <jason.kao@columbia.edu>
2021-03-26 17:36:32 -0400

    to make building work

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,5 +3,6 @@ module.exports = {
   plugins: ['snowpack-plugin-posthtml', '@snowpack/plugin-sass'],
   exclude: [
     '/Users/jasonkao/Development/redlining-heat/data/**/*.{tif,topojson,osm,json}',
+    '/Users/jasonkao/Development/redlining-heat/data/Makefile',
   ],
 };
