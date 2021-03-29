b01dab770defc3357879d6311cb31b41865f875d
Jason Kao <jason.kao@columbia.edu>
2021-03-29 08:20:56 -0400

    demo and stuff

--- data/scripts/download-temperatures.js
+++ data/scripts/download-temperatures.js
@@ -10,38 +10,35 @@ var runAnalysis = function () {
   });
 };
 
-const parameters = {
-  2020: []
+// Filter images by time and cloud cover
+// Map LST computation across images. Take the mean for overlaps
+function retrieveTemperatures(bbox, year) {
+  const [collection, lst_calc] = {
+    2020: [ee.ImageCollection('LANDSAT/LC08/C01/T1'), geet.lst_calc_ls8],
+    2000: [ee.ImageCollection('LANDSAT/LE07/C01/T1'), geet.lst_calc_ls7],
+  }[year];
+  return collection
+    .filterBounds(bbox)
+    .filterDate(year + '-06-01', year + '-08-31')
+    .sort('CLOUD_COVER')
+    .filter(ee.Filter.lte('CLOUD_COVER', 80))
+    .map(image =>
+      lst_calc(image)
+        .select('LST')
+        .multiply(9 / 5)
+        .add(32)
+        .rename(year),
+    )
+    .mean();
 }
 
 function main() {
   const bbox = ee.Geometry.Rectangle(eval(process.argv[2]));
-  const year = process.argv[3]
-
-  // Filter images by time and cloud cover
-  // Map LST computation across images. Take the mean for overlaps
-  const retrieveTemperatures = year => {
-    const [ collection, lst_calc ] = {
-      '2020': [ee.ImageCollection('LANDSAT/LC08/C01/T1'), geet.lst_calc_ls8],
-      '2000': [ee.ImageCollection('LANDSAT/LE07/C01/T1'), geet.lst_calc_ls7],
-    }[year]
-    return collection
-      .filterBounds(bbox)
-      .filterDate(year + '-06-01', year + '-08-31')
-      .sort('CLOUD_COVER')
-      .filter(ee.Filter.lte('CLOUD_COVER', 80))
-      .map(image =>
-        lst_calc(image)
-          .select('LST')
-          .multiply(9 / 5)
-          .add(32)
-          .rename(year),
-      )
-      .mean();}
+  const year = process.argv[3];
 
   // Stack all years into one image
   // const temporalTemperatures = retrieveTemperatures(2020).addBands(retrieveTemperatures(2000))
-  const temporalTemperatures = retrieveTemperatures(year)
+  const temporalTemperatures = retrieveTemperatures(bbox, year);
 
   // Mask the image
   var mask = ee
@@ -58,5 +55,5 @@ function main() {
   );
 }
 
-// Authenticate using a service account.
+// Authenticate using a service account and run
 ee.data.authenticateViaPrivateKey(privateKey, runAnalysis);
