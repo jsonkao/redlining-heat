3f2e3348aff582ce7b126da8fe735c12c4cd9992
Jason Kao <jason.kao@columbia.edu>
2021-03-31 12:40:40 -0400

    custom color configs

---
+++ data/scripts/create-color-config.py
@@ -0,0 +1,30 @@
+from osgeo.gdal import Warp
+from pathlib import Path
+import numpy as np
+import sys
+
+tempTifName = sys.argv[1]
+stem = Path(tempTifName).stem
+[city, year] = stem.split('-')
+
+# Cut temperature data according to city boundary
+ds = Warp(
+    "",
+    tempTifName,
+    format="VRT",
+    cutlineDSName=sys.argv[2],
+    cutlineWhere=f"city = '{city}'",
+    cropToCutline=True,
+)
+
+# Get temperature band
+band = ds.GetRasterBand(1)
+
+# Mask band according to NoData values
+values = np.ma.masked_equal(band.ReadAsArray(), band.GetNoDataValue())
+
+# Write out a color file based on valid values
+print(f'{values.min()} 0 0 255 255')
+print(f'{values.mean()} 255 255 255 255')
+print(f'{values.max()} 255 0 0 255')
+print('nv 236 236 236 255')
