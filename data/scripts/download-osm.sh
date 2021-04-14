f1f0029de8352aff7eb8b0d27599cde83b0801ef
Jason Kao <jason.kao@columbia.edu>
2021-04-14 10:01:49 -0400

    better script

--- data/scripts/download-osm.sh
+++ data/scripts/download-osm.sh
@@ -10,7 +10,16 @@ mkdir -p $outputDir
 for city in "$@"
 do
   outFile=$outputDir/$city.osm
-  [[ -e $outFile  ]] && continue
+  [[ -e osm-geojson/$city.geojson ]] && continue
+  if [ -e $outFile ]; then
+    if [ ! -s $outFile ]; then
+      rm $outFile
+    else
+      continue
+    fi
+  fi
   echo "=== Downloading $city ==="
-  wget -O $outputDir/$city.osm $query\&bbox=$(jq -r ".\"${city//_/ }\" | join(\",\")" city-bbox-index.json)
+  wget -O $outFile $query\&bbox=$(jq -r ".\"${city//_/ }\" | join(\",\")" city-bbox-index.json)
+  [[ $? -ne 0 ]] && rm $outFile
+  sleep 5
 done
