5cd96fc6e04dbf49ccb6e21eef5adca723167721
Jason Kao <jason.kao@columbia.edu>
2021-04-17 21:32:39 -0400

    t test

--- data/scripts/compare-impervious.py
+++ data/scripts/compare-impervious.py
@@ -5,6 +5,7 @@ in a particular city.
 
 from osgeo.gdal import Warp
 from pathlib import Path
+from scipy import stats
 import numpy as np
 import sys
 
@@ -39,8 +40,13 @@ temp_values = np.ma.masked_equal(temp_band.ReadAsArray(), temp_band.GetNoDataVal
 imp_band = ds_imp.GetRasterBand(1)
 imp_values = np.ma.masked_equal(imp_band.ReadAsArray(), imp_band.GetNoDataValue())
 
-temp_values.mask = np.ma.mask_or(temp_values.mask, imp_values.mask)
-print(temp_values.mask[100])
-temp_values.mask = np.ma.mask_or(temp_values.mask, ~imp_values.mask)
-print(temp_values.mask[100])
-# why is the second mask still all masked
+# Impervious and non impervious masks
+imp_mask = np.ma.mask_or(temp_values.mask, imp_values.mask)
+no_imp_mask = np.ma.mask_or(temp_values.mask, ~imp_values.mask)
+
+# T-test between temperatures on impervious surfaces vs. not on
+temp_imp = temp_values.copy()
+temp_imp.mask = imp_mask
+temp_values.mask = no_imp_mask
+result = stats.ttest_ind(temp_imp, temp_values, axis=None)
+print('t-test result:', result)
