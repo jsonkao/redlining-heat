ad052370266d6841cfc997726ec5dadb60547cb1
Jason Kao <jason.kao@columbia.edu>
2021-04-25 22:19:30 -0400

    tree export

---
+++ data/scripts/aggregate-histogram.py
@@ -0,0 +1,21 @@
+"""
+Aggregate frequency histogram to one value.
+Currently only used for tree cover task export.
+"""
+
+import sys
+import json
+
+for line in sys.stdin:
+    data = json.loads(line)
+    pixel_count = 0  # Track total count of pixels in feature
+    weighted_sum = 0  # Track weighted sum of percentages
+    hist = data["histogram"]
+    if len(hist) == 0:
+        continue
+    for pct in hist:
+        pixel_count += hist[pct]
+        weighted_sum += int(pct) * hist[pct]
+    print(
+        json.dumps({"id": data["id"], "pct_tree": round(weighted_sum / pixel_count, 3)})
+    )
